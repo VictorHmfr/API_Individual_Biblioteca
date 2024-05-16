0d9eeaead1954d35a65973346874451d814f6b41
VictorHmfr <vhmartinsfranca@gmail.com>
2024-05-15 22:02:29 -0300

    msg

---
+++ src/main/java/org/serratec/backend/apiindividual/ApiindividualApplication.java
@@ -0,0 +1,13 @@
+package org.serratec.backend.apiindividual;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiindividualApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiindividualApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/serratec/backend/apiindividual/ApiindividualApplicationTests.java
@@ -0,0 +1,13 @@
+package org.serratec.backend.apiindividual;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiindividualApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
